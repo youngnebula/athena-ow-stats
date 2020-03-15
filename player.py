13baf90bce3040e994e8cbb2bcb7fdac4d3f09a6
Ty Lovejoy <ty.lovejoy@g.austincc.edu>
2020-03-15 08:46:32 -0500

    fix

--- player.py
+++ player.py
@@ -62,16 +62,6 @@ class Player:
 
 
 
-if __name__ == "__main__":
-    player = Player("pc", "us", "pulsar-11413")
-    print('Name: ', player.get_name())
-    print('Level: ', player.get_total_level())
-    print('Rating: ', player.get_rating(), '-', player.get_rank())
-    print('Losses: ', player.get_loss())
-    print('Wins: ', player.get_win())    
-    player.display_all_ratings()
-
-
 
 
 
